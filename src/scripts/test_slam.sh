acff74121262bb23137821225c6a205d3ef4560e
nam <namkha.tech@gmail.com>
2020-04-16 20:49:12 +0000

    fixed shell script paths

--- src/scripts/test_slam.sh
+++ src/scripts/test_slam.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/workspace/catkin_ws/src/map/myworld.world " &
+xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/workspace/autonomous-service-bot/src/map/myworld.world " &
 sleep 5
 xterm  -e  " roslaunch turtlebot_teleop keyboard_teleop.launch" & 
 sleep 5
